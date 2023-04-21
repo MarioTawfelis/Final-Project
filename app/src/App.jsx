01713826c10a583149fc3ccbdb36d6a152468c3d
KseniiaEfremova <ef.xenya@yandex.ru>
2023-04-21 20:00:14 +0100

    Added basic styles, text on the home page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,8 +4,16 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="wrapper">
+      <nav>Logo, navbar</nav>
       Final project!!!!
+      <main>
+        <h1>Great style with a bad attitude.</h1>
+        <p>Penatibus sem vitae mollis luctus mi tellus. Maximus eu eleifend aptent dapibus metus maecenas consequat. Elementum interdum a semper. Netus nullam eros nisi volutpat nibh ex ultricies. Pharetra sagittis sit aliquet at. Magna nam platea justo.
+        </p>
+      </main>
+
+      <footer>Footer</footer>
     </div>
   )
 }
