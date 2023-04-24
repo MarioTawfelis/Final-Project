b4650d99362bcfb4dc2e9d4144d88bc110b5fedf
KseniiaEfremova <ef.xenya@yandex.ru>
2023-04-24 15:33:33 +0100

    Added product categories

---
+++ app/src/components/Categories.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import accessories from "../assets/images/bg-accessories.jpg";
+import clothing from "../assets/images/bg-clothing.jpg";
+import headphones from "../assets/images/bg-headphones.jpg";
+import home from "../assets/images/bg-home.jpg";
+import tech from "../assets/images/bg-tech.jpg";
+import essentials from "../assets/images/Essentials.avif";
+import jeans from "../assets/images/Jeans.webp";
+import Kitchen from "../assets/images/Kitchen.webp";
+import Arrivals from "../assets/images/Arrivals.jpg";
+import Shirts from "../assets/images/Shirts.webp";
+
+
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <h1>OUR PRODUCTS</h1>
+      <div className="grid-box">
+        <div className="category grid-item1">
+          <figure>
+            <img src={clothing} alt="bg-clothing img-category" />
+            <figcaption>
+              <h4>Activewear</h4>
+              <p>Ante mus blandit sapien sociosqu per consequat ad.</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div className="category grid-item2">
+          <figure>
+            <img src={home} alt="bg-home img-category" />
+            <figcaption>
+              <h4>Bed & Bath</h4>
+              <p>Ante mus blandit sapien sociosqu per consequat ad.</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div className="category grid-item3">
+          <figure>
+            <img src={accessories} alt="bg-clothing img-category" />
+            <figcaption>
+              <h4>Clearance</h4>
+              <p>Ante mus blandit sapien sociosqu per consequat ad.</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div className="category grid-item4">
+          <figure>
+            <img src={tech} alt="bg-home img-category" />
+            <figcaption>
+              <h4>Deals</h4>
+              <p>Ante mus blandit sapien sociosqu per consequat ad.</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div className="category grid-item5">
+          <figure>
+            <img src={essentials} alt="bg-home img-category" />
+            <figcaption>
+              <h4>Essentials</h4>
+              <p>Ante mus blandit sapien sociosqu per consequat ad.</p>
+            </figcaption>
+          </figure>
+        </div>
+
+        <div className="category grid-item6">
+          <figure>
+            <img src={jeans} alt="bg-home img-category" />
+            <figcaption>
+              <h4>Jeans</h4>
+              <p>Ante mus blandit sapien sociosqu per consequat ad.</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div className="category grid-item7">
+          <figure>
+            <img src={Kitchen} alt="bg-home img-category" />
+            <figcaption>
+              <h4>Kitchen & Dining</h4>
+              <p>Ante mus blandit sapien sociosqu per consequat ad.</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div className="category grid-item8">
+          <figure>
+            <img src={Arrivals} alt="bg-home img-category" />
+            <figcaption>
+              <h4>New Arrivals</h4>
+              <p>Ante mus blandit sapien sociosqu per consequat ad.</p>
+            </figcaption>
+          </figure>
+        </div>
+        <div className="category grid-item9">
+          <figure>
+            <img src={Shirts} alt="bg-home img-category" />
+            <figcaption>
+              <h4>Shirts</h4>
+              <p>Ante mus blandit sapien sociosqu per consequat ad.</p>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import MainText from "../components/MainText";
 import CaruselView from "../components/CaruselView";
 import "./home.css";
 import Collection from "../components/Collection";
+import Categories from "../components/Categories";
+import Description from "../components/Description";
 
 const Home = () => {
   return (
@@ -14,6 +16,12 @@ const Home = () => {
       <section>
         <Collection />
       </section>
+      <section>
+        < Categories />
+      </section>
+      <article>
+        < Description />
+      </article>
     </>
   );
 };
