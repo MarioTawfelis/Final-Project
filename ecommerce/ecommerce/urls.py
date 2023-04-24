7cd3636b14106013c13f51f8e55628ca822df3e9
Andrew Birch <andrewbirch1882@icloud.com>
2023-04-24 16:31:58 +0100

    drf spectacular working

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -16,14 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import (
-SpectacularAPIView,
-SpectacularSwaggerView
-)
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
-    path('api/docs', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs')
+    path('api/docs', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
+    path('users/', include('users.urls')), # include the users app's URLs
+    # ... include any other app-specific URLs here ...
 ]
--- ecommerce/users/views.py
+++ ecommerce/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# api/views.py
+from rest_framework import viewsets
+from .serializers import UserProfileSerializer
+from .models import UserProfile
 
-# Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
