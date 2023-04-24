cc40979a9a868a4d1f241d1244cc34bef96c5e67
Mario <mario.tawfelis@outlook.com>
2023-04-24 15:19:59 +0100

    Updated Product and Tag models and created tests

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -13,26 +13,19 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from os import environ
 
-
-
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get("SECRET_KEY")
-
+SECRET_KEY = environ.get("SECRET_KEY", "my-default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
 # Application definition
 
@@ -43,8 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'postgresql_db',
+    'ecommerce',
     'products',
+    'postgresql_db',
+
 ]
 
 MIDDLEWARE = [
@@ -78,14 +73,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
 
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': environ.get("DB_NAME"),
         'USER': environ.get("DB_USER"),
         'PASSWORD': environ.get("DB_PASSWORD"),
@@ -93,7 +87,6 @@ DATABASES = {
         'PORT': environ.get("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -113,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -124,7 +116,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- ecommerce/products/admin.py
+++ ecommerce/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from products.models import Product, Tag
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Tag)
+
