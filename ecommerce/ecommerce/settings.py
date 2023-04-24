0b56cf9c631f965dea92a234a788fcf565282108
Mario <mario.tawfelis@outlook.com>
2023-04-24 11:40:12 +0100

    Added Product model

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'postgresql_db',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
--- ecommerce/postgresql_db/models.py
+++ ecommerce/postgresql_db/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    sku = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    tags = models.ManyToManyField('Tag')
+    description = models.TextField()
+    has_sizes = models.BooleanField()
+    inventory_count = models.IntegerField()
+    retail_price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='product_image')
+    image_url = models.URLField()
+    rating = models.DecimalField(max_digits=2, decimal_places=1)
