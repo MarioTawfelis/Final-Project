16faab38c35b8f674d7c1622aff0d29c003b3de7
KseniiaEfremova <ef.xenya@yandex.ru>
2023-04-25 11:46:34 +0100

    Created inittial staff for login/logout

--- front-end/app/src/App.jsx
+++ front-end/app/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Home from './pages/Home'
+import Auth from './components/Auth'
 
 function App() {
-
+  
 
   return (
     <div className="wrapper">
+      {/* <div>
+        <Auth />
+      </div> */}
+      
       <nav>Logo, navbar</nav>
+
       <Home />
       
       <footer>Footer</footer>
