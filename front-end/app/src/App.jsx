4c1d07cbe9f09a24d91b4d270d56bd8c160aba6e
KseniiaEfremova <ef.xenya@yandex.ru>
2023-04-25 09:49:53 +0100

    Added initial staff

---
+++ front-end/app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+
+
+  return (
+    <div className="wrapper">
+      <nav>Logo, navbar</nav>
+      <Home />
+      
+      <footer>Footer</footer>
+    </div>
+  )
+}
+
+export default App
