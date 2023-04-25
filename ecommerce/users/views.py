16e476992b7255415f09d5db0a4e16f3f38cc155
HenryXanderTalent <henry.opara@xandertalent.com>
2023-04-25 07:59:59 +0100

    added crud methods for products and users

--- ecommerce/users/views.py
+++ ecommerce/users/views.py
@@ -2,7 +2,48 @@
 from rest_framework import viewsets
 from .serializers import UserProfileSerializer
 from .models import UserProfile
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
+    # queryset = UserProfile.objects.all()
+    # serializer_class = UserProfileSerializer
+
+    @api_view(['GET', 'POST'])
+    def user_list(request):
+        if request.method == 'GET':
+            users = UserProfile.objects.all()
+            serializer = UserProfileSerializer(users, many=True)
+            return Response(serializer.data)
+
+        elif request.method == 'POST':
+            serializer = UserProfileSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    @api_view(['GET', 'PUT', 'DELETE'])
+    def user_detail(request, pk):
+        try:
+            user = UserProfile.objects.get(pk=pk)
+        except UserProfile.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if request.method == 'GET':
+            serializer = UserProfileSerializer(user)
+            return Response(serializer.data)
+
+        elif request.method == 'PUT':
+            serializer = UserProfileSerializer(user, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        elif request.method == 'DELETE':
+            user.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
