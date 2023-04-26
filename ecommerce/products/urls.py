86c44126cb746734d459fdc2c6b1d287e59dc212
HenryXanderTalent <henry.opara@xandertalent.com>
2023-04-26 17:49:45 +0100

    get and delete by id now working for products

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -9,6 +9,7 @@ router.register(r'tags', TagViewSet)
 
 urlpatterns = [
     path('products/', views.product_list, name='product-list'),
+    path('products/<int:id>', views.product_detail, name='product_detail'),
     path('', include(router.urls)),
 ]
 
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -8,8 +8,8 @@ from rest_framework import status
 import json
 
 @api_view(['GET', 'POST'])
-def product_list(request, format=None):
-    if request.method == 'GET':
+def product_list(request, id=None, format=None):
+    if request.method == 'GET' and id is None:
         # get all the Products
         queryset = Product.objects.all()
         print(555)
@@ -22,10 +22,10 @@ def product_list(request, format=None):
         # or return in the browser admin version
         return Response(serializer_class.data)
 
-    elif request.method == 'POST':
+    elif request.method == 'POST' and id is None:
         serializer_class = ProductSerializer(data=request.data['fields'])
 
-        is_json(serializer_class)
+        #is_json(serializer_class)
         if serializer_class.is_valid():
             print('Serializer is valid')
             serializer_class.save()
@@ -34,16 +34,54 @@ def product_list(request, format=None):
             
             return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, id, format=None):
+   
+    if request.method == 'GET':
+        if id is not None:
+            try:
+                queryset = Product.objects.get(pk=id)
+            except Product.DoesNotExist:
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer_class = ProductSerializer(queryset)
+        return Response(serializer_class.data)
+
+    elif request.method == 'PUT':
+
+        try:
+            queryset = Product.objects.get(pk=id)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer_class = ProductSerializer(queryset, data=request.data)
+        if serializer_class.is_valid():
+            serializer_class.save()
+            return Response(serializer_class.data, status=status.HTTP_201_CREATED)
+        return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+
+        try:
+            queryset = Product.objects.get(pk=id)
+            serializer_class = ProductSerializer(queryset)
+            queryset.delete()
+            message = {'message': 'Product has been deleted'}
+            return Response(message, status=status.HTTP_204_NO_CONTENT)
+        except Product.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 
-def is_json(value):
-    try:
-        json.loads(value)
-        print(222)
-    except (ValueError, TypeError):
-        print(111)
-        return False
-    return True
+
+
+# def is_json(value):
+#     try:
+#         json.loads(value)
+#         print(222)
+#     except (ValueError, TypeError):
+#         print(111)
+#         return False
+#     return True
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -52,29 +90,6 @@ class ProductViewSet(viewsets.ModelViewSet):
     basename = 'product'
 
 
-    @api_view(['GET', 'PUT', 'DELETE'])
-    def product_detail(request, id, format=None):
-
-        try:
-            queryset = Product.objects.get(pk=id)
-        except Product.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        if request.method == 'GET':
-            serializer_class = ProductSerializer(queryset)
-            return Response(serializer_class.data)
-
-        elif request.method == 'PUT':
-            serializer_class = ProductSerializer(queryset, data=request.data)
-            if serializer_class.is_valid():
-                serializer_class.save()
-                return Response(serializer_class.data, status=status.HTTP_201_CREATED)
-            return Response(serializer_class.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        elif request.method == 'DELETE':
-            serializer_class = ProductSerializer(queryset)
-            queryset.delete()
-            return Response(serializer_class.data, status=status.HTTP_204_NO_CONTENT)
 
 
 class TagViewSet(viewsets.ModelViewSet):
