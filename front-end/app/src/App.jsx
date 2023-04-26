f3e32ef2e5ca375f3e022a7bfb5e6e42701804f6
KseniiaEfremova <ef.xenya@yandex.ru>
2023-04-26 12:14:20 +0100

    Imported router packages

--- front-end/app/src/App.jsx
+++ front-end/app/src/App.jsx
@@ -1,18 +1,17 @@
-import './App.css'
-import Home from './pages/Home'
-import Auth from './components/Auth'
-import NavBar from './components/Navbar'
+import "./App.css";
+import Home from "./pages/Home";
+import Auth from "./components/Auth";
+import NavBar from "./components/Navbar";
+
 
 function App() {
-  
-
   return (
     <div className="wrapper">
       {/* <Auth /> */}
       <NavBar />
       <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
